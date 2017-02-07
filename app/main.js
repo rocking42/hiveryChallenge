b44fe23bd0c3870ed9cea18cad672a44ddc0b765
rocking42 <nedpike3@gmail.com>
2017-02-08 10:36:49 +1100

    Added JSON loading

--- app/main.js
+++ app/main.js
@@ -1,1 +1,9 @@
-[1,2,3,4].reduce((prev, next) => prev + next)
+const d3_request = require("d3-request");
+import {
+  sum
+} from "./dataSort";
+
+d3_request.json("../assets/data/register.json", (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+});
